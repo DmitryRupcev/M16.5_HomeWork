d8473b9d3f8b2b10e490a0a762235e8857727a96
rupcevDmitry <rupcev.dmitry@icloud.com>
2024-03-09 18:11:46 +0300

    added file Constants.swift

---
+++ 16.5_Practical_work/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  16.5_Practical_work
+//
+//  Created by Дмитрий Рупцев on 09.03.2024.
+//
+
+import Foundation
+import UIKit
+
+enum Constants {
+    
+    enum Colors {
+        
+    }
+}
