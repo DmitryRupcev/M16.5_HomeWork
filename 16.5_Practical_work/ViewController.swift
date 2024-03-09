8f738b3b684c178781db524c71d16a32c416e3c0
rupcevDmitry <rupcev.dmitry@icloud.com>
2024-03-09 17:58:52 +0300

    first commit

---
+++ 16.5_Practical_work/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  16.5_Practical_work
+//
+//  Created by Дмитрий Рупцев on 09.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
